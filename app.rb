ee73fb94f5ea17a7f0bd6a98823f04e088788e3d
Prabu Sathan <prabu.sathan@gmail.com>
2017-05-22 17:49:37 +0100

    modified app.rb

--- app.rb
+++ app.rb
@@ -13,6 +13,13 @@ get '/dog' do
   'german'
 end
 
-get '/cat' do
+get '/random-cat' do
+  @cat_name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params[:name]
+  @cat_name = params[:name]
   erb(:index)
 end
