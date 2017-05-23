e2e02ed3cb2596a6a81321a318a844be87185681
Simon Ashbery <si.ashbery@gmail.com>
2017-05-23 12:12:28 +0100

    Fixed the form

--- app.rb
+++ app.rb
@@ -19,13 +19,6 @@ get '/random-cat' do
 end
 
 get '/named-cat' do
-  p params[:name]
   @cat_name = params[:name]
-  erb(:index)
-end
-
-get '/form-cat' do
-
-  @cat_name = erb(:name_form)
   erb(:index)
 end
